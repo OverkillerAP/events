cf45e4799a13af4cf5513a6f8315bc1199800443
Overkiller <artjomspavlovs@gmail.com>
2026-01-06 16:29:55 +0200

    #

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -10,8 +11,12 @@ app.get('/event/:eventId', (req, res) => {
     res.send(`Event! ${req.params.eventId}`);
 });
 
-
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+
+const root = path.join(__dirname, 'public');
+app.use(express.static(root));
+
+app.listen(3000, () => console.log('Server running on port 3000'));
