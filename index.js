33bef9a90d8ad9ec0bfa2b59f86bb7029d47c6fa
Overkiller <artjomspavlovs@gmail.com>
2026-01-07 14:29:32 +0200

    #

--- index.js
+++ index.js
@@ -8,20 +8,45 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Статическая папка
+// view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+// Static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Главная страница
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+//app.get('/', (req, res) => {
+   // res.sendFile(path.join(__dirname, 'public', 'index.html'));
+//});
 
-// Страница события
+// Event lpp
 app.get('/event/:eventId', (req, res) => {
     res.send(`Event! ${req.params.eventId}`);
 });
 
-// Запуск сервера
+// Server Run
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
+});
+
+// Date
+const menu = [
+  {name: 'Все', category: 'all'},
+  {name: 'Музыка', category: 'music'},
+  {name: 'Новости', category: 'news'},
+  {name: 'Технологии', category: 'tech'},
+  {name: 'Спорт', category: 'sports'},
+  {name: 'Фильмы', category: 'movies'},
+  {name: 'Образование', category: 'education'}
+];
+app.get('/', (req, res) => {
+  res.render('index', {
+    title: 'Prakse-Projekts',
+    logo: '/images/logo.png',
+    menu,
+    mainTitle: 'Основной контент',
+    mainContent: 'Это динамически сгенерированный контент с сервера.',
+    year: new Date().getFullYear()
+  });
 });
---
+++ test.js
@@ -0,0 +1,3 @@
+const { MongoClient } = require('mongodb');
+
+console.log("MongoDB module loaded successfully");
